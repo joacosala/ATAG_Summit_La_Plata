30df58f78e14330f8bec9d1fe437c4340fec7323
summit-user <summit-user@accenture.com>
2012-05-29 15:24:08 -0500

    add git ingnore file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ATAGSummitJS::Application.config.secret_token = '217d4795651e46f718a3b903c775962aee38c65149c3ecd070c9c7646ff882500562797b635384eb4f8579314044ba50d92d8d071f9066d79100b64c18df83ca'
